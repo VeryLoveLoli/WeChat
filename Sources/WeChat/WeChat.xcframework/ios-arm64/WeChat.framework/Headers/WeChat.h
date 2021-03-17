fe53353b87f37d9bf896c8238af798f4c4a92f66
VeryLoveLoli <chuancan@icloud.com>
2021-03-17 11:25:57 +0800

    更新

--- Sources/WeChat/WeChat.xcframework/ios-arm64/WeChat.framework/Headers/WeChat.h
+++ Sources/WeChat/WeChat.xcframework/ios-arm64/WeChat.framework/Headers/WeChat.h
@@ -15,6 +15,10 @@ FOUNDATION_EXPORT const unsigned char WeChatVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WeChat/PublicHeader.h>
 
+#import <Security/Security.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <WebKit/WebKit.h>
+
 #import <WeChat/WXApi.h>
 #import <WeChat/WXApiObject.h>
 #import <WeChat/WechatAuthSDK.h>
