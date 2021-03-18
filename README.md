e6d7d36464fca0edbfc1694bb941448fa72b05dd
VeryLoveLoli <chuancan@icloud.com>
2021-03-18 15:58:45 +0800

    更新

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 微信SDK XCFramework SwiftPackage
 
+### 工程引用
 在 `Link Binary With Libraries` 添加 `libc++.tbd`
-
 在 `Other Linker Flags` 添加 `-ObjC`、`-all_load`或`-force_load` 
+
+### SwiftPackage 引用
+
+```swift
+.target(
+    name: "Name",
+    dependencies: ["WeChat"],
+    linkerSettings: [.linkedLibrary("c++")]),
+```
--- Sources/WeChat/WeChat.xcframework/ios-i386_x86_64-simulator/WeChat.framework/Headers/WeChat.h
+++ Sources/WeChat/WeChat.xcframework/ios-i386_x86_64-simulator/WeChat.framework/Headers/WeChat.h
@@ -15,10 +15,17 @@ FOUNDATION_EXPORT const unsigned char WeChatVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WeChat/PublicHeader.h>
 
+/**
+ 在 `Link Binary With Libraries` 添加 `libc++.tbd`
+ 在 `Other Linker Flags` 添加 `-ObjC`、`-all_load`或`-force_load`
+ */
+
+/// 系统库
 #import <Security/Security.h>
 #import <CoreGraphics/CoreGraphics.h>
 #import <WebKit/WebKit.h>
 
+/// 微信头文件
 #import <WeChat/WXApi.h>
 #import <WeChat/WXApiObject.h>
 #import <WeChat/WechatAuthSDK.h>
